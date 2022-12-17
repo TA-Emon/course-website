963bfc3ac4f21b33a342861cb0dd2a0296d2876b
TA-Emon <tanviremon726@gmail.com>
2022-12-17 19:52:47 +0600

    course data loaded and all course has shown in course page

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SingleCourse from '../SingleCourse/SingleCourse';
 import './Course.css';
 
 const Course = () => {
+
+    const [course, setCourse] = useState([]);
+    useEffect(() => {
+        fetch('course.json')
+            .then(res => res.json())
+            .then(data => setCourse(data))
+    }, []);
+
     return (
-        <div>
-            <h1>Course</h1>
+        <div className="course">
+            <div className='container course-wrapper'>
+                {
+                    course.map(course => <SingleCourse key={course.id} course={course}></SingleCourse>)
+                }
+            </div>
         </div>
+
     );
 };
 
---
+++ src/components/SingleCourse/SingleCourse.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SingleCourse.css';
+
+const SingleCourse = (props) => {
+    console.log(props);
+    const {img, price, mentor, course} = props.course;
+    return (
+        <div className='course-item'>
+            <img className='course-img' src={img} alt="" />
+            <p className='course-name'>Course: {course}</p>
+            <b className='mentor'>Mentor: {mentor}</b>
+            <span className='price'>${price}</span>
+        </div>
+    );
+};
+
+export default SingleCourse;
